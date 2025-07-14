83f8651919eb42e7b858e20e68962a0130981abd
Shaimaa-Mady <shaimaa.mady99@gmail.com>
2025-07-14 07:16:47 +0300

    set global variables and design home component

---
+++ src/app/components/testmonial/testmonial.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testmonial',
+  imports: [],
+  templateUrl: './testmonial.component.html',
+  styleUrl: './testmonial.component.css'
+})
+export class TestmonialComponent {
+
+}
