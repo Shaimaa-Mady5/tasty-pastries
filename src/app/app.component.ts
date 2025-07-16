3cbcbc1d917fae3dfebb9cddb31f58ca7cfb4505
Shaimaa-Mady <shaimaa.mady99@gmail.com>
2025-07-16 03:35:49 +0300

    feature/contact: designed form and the right side of the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { TestmonialComponent } from "./components/testmonial/testmonial.component";
+import { ContactComponent } from "./components/contact/contact.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent, TestmonialComponent],
+  imports: [HomeComponent, TestmonialComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
